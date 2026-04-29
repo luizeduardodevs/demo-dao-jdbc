61728c9cb191e3b22446823d844bef512964b2d8
Luiz Eduardo <luizeduardoledsm@gmail.com>
2026-04-29 03:10:51 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Departament;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args){
         Departament obj = new Departament(1, "books");
-        System.out.println(obj);
+        Seller seller = new Seller(21,"bob","bob@",new Date(),3000.00,obj);
+        System.out.println(seller);
 
 
     }
