8966590eb35e9775b463e419b0ff52d73f347fb7
Luiz Eduardo <luizeduardoledsm@gmail.com>
2026-05-05 03:23:26 -0300

    findAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,11 +14,16 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("\n=== TEST 1 seller findById ===");
+        System.out.println("\n=== TEST 2 seller findById ===");
         Departament departament = new Departament(2,null);
         List<Seller> list = sellerDao.findByDepartment(departament);
         for (Seller oj: list){
             System.out.println(oj);
+        }
+        System.out.println("\n=== TEST 3 seller findAll ===");
+        list = sellerDao.findAll();
+        for (Seller obj: list){
+            System.out.println(obj);
         }
 
     }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -88,7 +88,42 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return List.of();
+        PreparedStatement st =null;
+        ResultSet rs = null; // se esse resultset estiver sendo apontado para posição zero, ele não contem objeto
+        try {
+            //retornando um vendedor por ID
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            +"FROM seller INNER JOIN department "
+                            +"ON seller.DepartmentId = department.Id "
+                            +"ORDER BY Name ");
+            rs = st.executeQuery();//o resultado vai cair dentro do resultSet
+            List<Seller> list = new ArrayList<>();
+            //controlado a nao repetição de departamento, pois cada vendedor so tem um departamento.
+            Map<Integer,Departament>map =new HashMap<>();
+            while (rs.next()){// uso do while pra percorre enquanto tiver um próximo valor
+                //testando se ele ja departamento ja existe
+                Departament dep =map.get(rs.getInt("DepartmentId"));//aqui retorna os valores guardado dentro do map
+
+                //na primeira vez que comecar a ler vai cair no if, porque o dep ainda esta vazio
+                //e nas outras leituras vai cair dentro do map.get;
+                if(dep == null){
+                    dep = instantiateDepartment(rs);//cria o objeto
+                    map.put(rs.getInt("DepartmentId"), dep);//joga denro do map
+                }
+
+                Seller obj = instantieSeller(rs,dep);
+                //dentro da classe SELLER tem a injenção de departamento.
+                list.add(obj);//retorna o objeto seller
+            }
+            return list;//se não retorna nada, vai cair como nulo
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+        }
     }
 
     @Override
