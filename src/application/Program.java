bb4dac23a190845775577a731a9c05583f761b83
Luiz Eduardo <luizeduardoledsm@gmail.com>
2026-05-06 19:38:28 -0300

    Update implemetantion

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,7 +12,7 @@ public class Program {
     public static void main(String[] args){
         SellerDao sellerDao = DaoFactory.createSellerDao();//voce esconde aonde esta a implementação, e so mostra a interface, injeção de depedência
         System.out.println("=== TEST 1 seller findById ===");
-        Seller seller = sellerDao.findById(3);
+        Seller seller = sellerDao.findById(3);//foi reaproveitada no update
         System.out.println(seller);
 
         System.out.println("\n=== TEST 2 seller findById ===");
@@ -30,5 +30,11 @@ public class Program {
         Seller seller1 = new Seller(null,"Greg","greg@gmail.com", new Date(),4000.00,departament);//aproveitou o objeto departament
         sellerDao.insert(seller1);//passa o parametro da interface
         System.out.println("Inserted! new id: "+ seller1.getId());
+
+        System.out.println("\n=== TEST 5 seller UPDATE ===");
+        seller = sellerDao.findById(1);//seller vai receber a pesquisa de encontro pelo id = 1
+        seller.setName("Martha Waine");//modificou o nome
+        sellerDao.update(seller);//e dentro da atualização passou seller pra atualizar os dados.
+        System.out.println("Update completed");
     }
 }
